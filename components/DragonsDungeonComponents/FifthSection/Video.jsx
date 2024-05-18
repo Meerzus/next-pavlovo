20841b9a5dfc21a5026148f47e5e4b0cb83c0c0d
鬼鮫 <mmursaliev00@gmail.com>
2024-05-18 16:02:02 +0500

    - final

--- components/DragonsDungeonComponents/FifthSection/Video.jsx
+++ components/DragonsDungeonComponents/FifthSection/Video.jsx
@@ -1,11 +1,36 @@
 'use client'
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function Video({width, height, igrolend}) {
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+    const [videoSrc, setVideoSrc] = useState('')
+
+    useEffect(() => {
+        setScreenWidth(window.innerWidth)
+    }, []);
+
+    useEffect(() => {
+        if (!igrolend) {
+            setVideoSrc('/video.mp4')
+        } else if (igrolend && screenWidth > 1200) {
+            setVideoSrc('/igrolendVideo-full.mp4')
+        } else if (igrolend && screenWidth <= 1200) {
+            setVideoSrc('/igrolendVideo.mp4')
+        }
+    }, [screenWidth]);
+
+    console.log(videoSrc)
+
     return (
         <video width={width} height={height} controls muted>
-            <source src={igrolend ? '/igrolendVideo.mp4' : "/video.mp4"} type="video/mp4"/>
+            {
+                videoSrc &&
+                <source src={videoSrc} type="video/mp4"/>
+            }
+            {/*{!igrolend && <source src={'/video.mp4'} type="video/mp4"/>}*/}
+            {/*{(screenWidth > 1200 && igrolend) && <source src={'/igrolendVideo-full.mp4'} type="video/mp4"/>}*/}
+            {/*{(screenWidth <= 1200 && igrolend) && <source src={'/igrolendVideo.mp4'} type="video/mp4"/>}*/}
             Your browser does not support the video tag.
         </video>
     );
